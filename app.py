06773e1395fac3dd39a0a62e0511cdaf44ea6b73
felix-gy <felix.gordillo@ucsp.edu.pe>
2022-09-19 18:12:41 -0500

    Tarea

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/curriculum", methods=["POST"])
+def info():
+    names = request.form.get('nombre')
+    birth_date =request.form.get('fecha')
+    ocupation = request.form.get('ocupacion')
+    contact = request.form.get('contacto')
+    nationality = request.form.get('nacionalidad')
+    ingles = request.form.get('ingles')
+    lenguajes = request.form.get('lenguajes')
+    aptitud = request.form.get('aptitud')
+    habilidades = request.form.get('habilidades')
+    perfil = request.form.get('perfil')
+    return render_template('curriculum.html', 
+        perfil=perfil,
+        habilidades=habilidades,
+        aptitud=aptitud,
+        lenguajes=lenguajes,
+        ingles=ingles,
+        nationality=nationality,
+        contact=contact,
+        ocupation=ocupation,
+        birth_date=birth_date,
+        names=names,
+        )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
