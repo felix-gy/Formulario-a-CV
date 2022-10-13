d15701a293bb9d27acf86ebaf1defb9f2667607c
Felix Gordillo <felix.gordillo@ucsp.edu.pe>
2022-10-13 09:15:57 -0500

    añadiendo funcionalidad botones añadir

--- static/curriculum.js
+++ static/curriculum.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector()
 });
 
 function añadir_1 (a) {
@@ -10,4 +9,21 @@ function añadir_1 (a) {
     lista.appendChild(li);
     input.value = '';
     input.focus();
+}
+function añadir_2 (a) {
+    const input = a.previousElementSibling;
+    const textArea = a.nextElementSibling;
+    const div = document.createElement('div');
+    const h4 = document.createElement('h4');
+    const p = document.createElement('p');
+    const lista = input.previousElementSibling;
+    h4.innerHTML += input.value;
+    p.innerHTML = textArea.value;
+    div.appendChild(h4);
+    div.appendChild(p);
+    lista.appendChild(div);
+    input.value = '';
+    input.focus();
+    textArea.value = '';
+    textArea.focus();
 }
