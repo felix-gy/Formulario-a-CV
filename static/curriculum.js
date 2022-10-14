06751b679c9dcf40614f2022439e5b5e384b14f1
Felix Gordillo <felix.gordillo@ucsp.edu.pe>
2022-10-14 16:35:57 -0500

    css changes

--- static/curriculum.js
+++ static/curriculum.js
@@ -13,15 +13,15 @@ function añadir_1 (a) {
 function añadir_2 (a) {
     const input = a.previousElementSibling;
     const textArea = a.nextElementSibling;
-    const div = document.createElement('div');
+    const section = document.createElement('section');
     const h4 = document.createElement('h4');
     const p = document.createElement('p');
     const lista = input.previousElementSibling;
     h4.innerHTML += input.value;
     p.innerHTML = textArea.value;
-    div.appendChild(h4);
-    div.appendChild(p);
-    lista.appendChild(div);
+    section.appendChild(h4);
+    section.appendChild(p);
+    lista.appendChild(section);
     input.value = '';
     input.focus();
     textArea.value = '';
