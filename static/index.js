6935b5562d69bb2719372753381a82c2e52371d8
Felix Gordillo <felix.gordillo@ucsp.edu.pe>
2022-10-22 12:48:26 -0500

    trabajo dbp

--- static/index.js
+++ static/index.js
@@ -1,14 +1,15 @@
+const submit = document.querySelector("#submit");
+
 document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector("#submit").disabled = true;
+	submit.disabled = true;
 });
 
 function validate() {
-	document.querySelector('#submit').disabled  = false;
+	submit.disabled  = false;
 
 	document.querySelectorAll('.check_empty').forEach(input => {
-		console.log(input.value.length);
 		if(input.value.length == 0 ){
-			document.querySelector('#submit').disabled =  true;
+			submit.disabled =  true;
 		}
-	})
+	});
 }
